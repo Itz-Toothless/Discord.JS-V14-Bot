4821352f7cf760cd73d56cd977b1f6eec47d0a14
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-08-18 00:37:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+app.listen(port, () => console.log(`${client.user.tag} listening at http://localhost:${port}`));
 
 const client = new Client({
     intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildPresences]
