6ea9eecf025584036b33770260cba6571200c099
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-08-18 00:33:29 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -13,41 +13,13 @@ module.exports = {
                 .setTitle('Bot Stats')
                 .setDescription('**Here are some stats about the Bot**')
                 .setThumbnail(`${client.user.displayAvatarURL({ dynamic: true })}`)
-                .addFields({
-                    name: 'Bot Name',
-                    value: client.user.tag,
-                    inline: true
-                },
-                {
-                    name: 'Bot ID',
-                    value: client.user.id,
-                    inline: true
-                },
-                {
-                    name: 'Bot Version',
-                    value: '1.0.0 Alpha',
-                    inline: true
-                },
-                {
-                    name: 'Bot Prefix',
-                    value: '/',
-                    inline: true
-                },
-                {
-                    name: 'Bot Creator',
-                    value: 'Itz_Toothless#8135',
-                    inline: true
-                },
-                {
-                    name: 'Bot Created At',
-                    value: `**<t:${Math.round(parseInt(client.user.createdTimestamp) / 1000)}:F>**`,
-                    inline: true
-                },
-                {
-                    name: 'Bot Uptime',
-                    value: `**<t:${Math.round(parseInt(client.readyTimestamp) / 1000)}:R>**`,
-                    inline: true
-                })
+                .addFields({ name: 'Bot Name', value: client.user.tag, inline: true },
+                    { name: 'Bot ID', value: client.user.id, inline: true },
+                    { name: 'Bot Version', value: '1.0.0', inline: true },
+                    { name: 'Bot Prefix', value: '/', inline: true },
+                    { name: 'Bot Creator', value: 'Itz_Toothless#8135', inline: true },
+                    { name: 'Bot Created At', value: `**<t:${Math.round(parseInt(client.user.createdTimestamp) / 1000)}:F>**`, inline: true },
+                    { name: 'Bot Uptime', value: `**<t:${Math.round(parseInt(client.readyTimestamp) / 1000)}:R>**`, inline: true })
                 .setTimestamp()
                 .setFooter({ text: 'Made with ❤️ by Itz_Toothless#8135', iconURL: client.user.displayAvatarURL({ dynamic: true }) });
             return await interaction.reply({ embeds: [statsEmbed] });
