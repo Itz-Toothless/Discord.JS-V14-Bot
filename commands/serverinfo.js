934df56ff6503187c7dbf78488dbbbe00e077ca0
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-08-18 00:32:21 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -15,36 +15,12 @@ module.exports = {
                 .setTitle('Server Stats')
                 .setDescription('**Here are some stats about the Server**')
                 .setThumbnail(`${server.iconURL({ dynamic: true })}`)
-                .addFields({
-                    name: 'Server Name',
-                    value: server.name,
-                    inline: true
-                },
-                {
-                    name: 'Server ID',
-                    value: server.id,
-                    inline: true
-                },
-                {
-                    name: 'Server Owner',
-                    value: owner.user.tag,
-                    inline: true
-                },
-                {
-                    name: 'Server Created At',
-                    value: `**<t:${Math.round(parseInt(server.createdTimestamp) / 1000)}:F>**`,
-                    inline: true
-                },
-                {
-                    name: 'Server Region',
-                    value: server.preferredLocale,
-                    inline: true
-                }, 
-                {
-                    name: 'Server Verification Level',
-                    value: server.verificationLevel,
-                    inline: true
-                })
+                .addFields({ name: 'Server Name', value: server.name, inline: true },
+                    { name: 'Server ID', value: server.id, inline: true },
+                    { name: 'Server Owner', value: owner.user.tag, inline: true },
+                    { name: 'Server Created At', value: `**<t:${Math.round(parseInt(server.createdTimestamp) / 1000)}:F>**`, inline: true },
+                    { name: 'Server Region', value: server.preferredLocale, inline: true },
+                    { name: 'Server Verification Level', value: server.verificationLevel, inline: true })
                 .setTimestamp()
                 .setFooter({ text: 'Made with ❤️ by Itz_Toothless#8135', iconURL: client.user.displayAvatarURL({ dynamic: true }) });
             return await interaction.reply({ embeds: [serverEmbed] });
