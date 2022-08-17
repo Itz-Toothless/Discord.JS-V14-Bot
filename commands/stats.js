a1dd8936b7bb06c57f4a6232a7ee27da03537e17
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-08-18 00:19:47 +0200

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -18,36 +18,36 @@ module.exports = {
                     value: client.user.tag,
                     inline: true
                 },
-                    {
-                        name: 'Bot ID',
-                        value: client.user.id,
-                        inline: true
-                    },
-                    {
-                        name: 'Bot Version',
-                        value: '1.0.0',
-                        inline: true
-                    },
-                    {
-                        name: 'Bot Prefix',
-                        value: '/',
-                        inline: true
-                    },
-                    {
-                        name: 'Bot Creator',
-                        value: 'Itz_Toothless#8135',
-                        inline: true
-                    },
-                    {
-                        name: 'Bot Created At',
-                        value: `**<t:${Math.round(parseInt(client.user.createdTimestamp) / 1000)}:F>**`,
-                        inline: true
-                    },
-                    {
-                        name: 'Bot Uptime',
-                        value: `**<t:${Math.round(parseInt(client.readyTimestamp) / 1000)}:R>**`,
-                        inline: true
-                    })
+                {
+                    name: 'Bot ID',
+                    value: client.user.id,
+                    inline: true
+                },
+                {
+                    name: 'Bot Version',
+                    value: '1.0.0 Alpha',
+                    inline: true
+                },
+                {
+                    name: 'Bot Prefix',
+                    value: '/',
+                    inline: true
+                },
+                {
+                    name: 'Bot Creator',
+                    value: 'Itz_Toothless#8135',
+                    inline: true
+                },
+                {
+                    name: 'Bot Created At',
+                    value: `**<t:${Math.round(parseInt(client.user.createdTimestamp) / 1000)}:F>**`,
+                    inline: true
+                },
+                {
+                    name: 'Bot Uptime',
+                    value: `**<t:${Math.round(parseInt(client.readyTimestamp) / 1000)}:R>**`,
+                    inline: true
+                })
                 .setTimestamp()
                 .setFooter({ text: 'Made with ❤️ by Itz_Toothless#8135', iconURL: client.user.displayAvatarURL({ dynamic: true }) });
             return await interaction.reply({ embeds: [statsEmbed] });
