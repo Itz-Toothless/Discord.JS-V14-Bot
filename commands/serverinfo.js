d87adcfc0f200bdfba6560fa4d6fc730f25b0fb6
Itz_Toothless <80362356+Itz-Toothless@users.noreply.github.com>
2022-08-23 15:55:18 +0200

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -10,9 +10,11 @@ module.exports = {
             let server = interaction.guild
             let owner = server.ownerId
             let theOwner = await interaction.guild.members.fetch(owner)
+            let verifLevels = ["None", "Low", "Medium", "(╯°□°）╯︵  ┻━┻", "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻"];
+            let verif = verifLevels[server.verificationLevel]
             let serverEmbed = new EmbedBuilder()
                 .setColor(0x7289DA)
-                .setAuthor({ name: `${interaction.member.tag}`, iconURL: `${interaction.member.displayAvatarURL({ dynamic: true })}` })
+                .setAuthor({ name: `${interaction.user.tag}`, iconURL: `${interaction.member.displayAvatarURL({ dynamic: true })}` })
                 .setTitle('Server Stats')
                 .setDescription('**Here are some stats about the Server**')
                 .setThumbnail(`${server.iconURL({ dynamic: true })}`)
@@ -21,7 +23,7 @@ module.exports = {
                     { name: 'Server Owner', value: `${theOwner.user.tag}\n${theOwner.id}`, inline: true },
                     { name: 'Server Created At', value: `**<t:${Math.round(parseInt(server.createdTimestamp) / 1000)}:F>**`, inline: true },
                     { name: 'Server Region', value: `${server.preferredLocale}`, inline: true },
-                    { name: 'Server Verification Level', value: `${server.verificationLevel}`, inline: true })
+                    { name: 'Server Verification Level', value: `${verif}`, inline: true })
                 .setTimestamp()
                 .setFooter({ text: 'Made with ❤️ by Itz_Toothless#8135', iconURL: client.user.displayAvatarURL({ dynamic: true }) });
             return await interaction.reply({ embeds: [serverEmbed] });
